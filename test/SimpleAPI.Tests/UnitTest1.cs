156020b8d1e41ec3de81976d91b5bcf92778c44d
Karl Thomas <karlthomas23@gmail.com>
2022-03-01 16:21:54 +1030

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.Logging;
+using SimpleAPI.Controllers;
+using Xunit;
+
+namespace SimpleAPI.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+        var returnValue = controller.Get(1);
+        Assert.Equal("Les Jackson", returnValue.Value);
+    }
+}
